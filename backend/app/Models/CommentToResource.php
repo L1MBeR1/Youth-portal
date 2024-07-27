2d11f381a004bfef130f32fed606fb2367d7bd30
dsherstyannikov <dsherstyannikov@yandex.ru>
2024-07-27 17:41:20 +0800

    фикс

--- backend/app/Models/CommentToResource.php
+++ backend/app/Models/CommentToResource.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class CommentToResource extends Pivot
 {
     protected $table = 'comment_to_resource';
+
+    public $timestamps = false;
     protected $guarded = [];
     use HasFactory;
 }

517c2da2aec43b5ad148b7e2133345aa5ec5ca35
dsherstyannikov <dsherstyannikov@yandex.ru>
2024-08-02 16:52:24 +0800

    fix

--- backend/routes/api/users.php
+++ backend/routes/api/users.php
@@ -6,22 +6,12 @@ use App\Http\Controllers\API\SUController;
 
 // Работа с пользователями
 Route::group([
-    'middleware' => ['auth:api', 'role:admin'],
+    'middleware' => ['auth:api'],
     'prefix' => 'users'
 ], function () {
     Route::get('', [UserController::class, 'listUsers']);
     Route::post('roles', [UserController::class, 'updateUserRoles']);
     Route::delete('{user_id}/roles/{role_name}', [UserController::class, 'deleteRoleFromUser']);
-    Route::patch('{user_id}/block', [SUController::class, 'blockUser']);
-});
-
-Route::group([
-    'middleware' => ['auth:api'],
-    'prefix' => 'users'
-], function () {
-    // Route::get('', [UserController::class, 'listUsers']);
-    // Route::post('roles', [UserController::class, 'updateUserRoles']);
-    // Route::delete('{user_id}/roles/{role_name}', [UserController::class, 'deleteRoleFromUser']);
     Route::patch('{user_id}/block', [SUController::class, 'blockUser']);
     Route::patch('{user_id}/unblock', [SUController::class, 'unblockUser']);
 });

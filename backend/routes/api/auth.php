cb42d7ea31da0e413015fa1d5bd5f69facf2390d
dsherstyannikov <dsherstyannikov@yandex.ru>
2024-09-07 18:38:41 +0800

    Подтверждение при удалении своего аккаунта

--- backend/routes/api/auth.php
+++ backend/routes/api/auth.php
@@ -8,6 +8,7 @@ Route::group([
     'middleware' => ['api'],
     'prefix' => 'auth'
 ], function () {
+    
     Route::get('verify_email', [AuthController::class, 'verifyEmail'])->withoutMiddleware('auth');
     Route::get('change_password', [AuthController::class, 'changePassword'])->withoutMiddleware('auth');
     

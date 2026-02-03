0a692fead46e90d50786fcae08cc76f4e8c082b3
dsherstyannikov <dsherstyannikov@yandex.ru>
2026-02-03 17:58:22 +0800

    fix

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Application;
 use App\Http\Middleware\CorsMiddleware;
 use App\Http\Middleware\CheckUserBlocked;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Support\Facades\Route;
 // use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,13 +13,9 @@ use PHPOpenSourceSaver\JWTAuth\Http\Middleware\Authenticate as JWTAuthenticate;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: function () {
-            Route::prefix('api')  // <-- Добавляем префикс
-                ->middleware('api')
-                ->group(base_path('routes/api.php'));
-        },
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up'
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
